e30249f656ce34e6b2a75ddbe7cc6ac6bed84f9d
gashboy9 <gashboy@yahoo.com>
2021-03-12 19:05:22 +0000

    Question completed with sample data

--- src/assignment/Q1.java
+++ src/assignment/Q1.java
@@ -4,7 +4,12 @@ public class Q1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Author author = new Author("Gabriel","gabriel@gmail.com",'m');
+		Book book = new Book("Toby's Story", author, 56.24, 1555);
+		
+		System.out.println(author);
+		System.out.println(book);
 
 	}
 
@@ -47,40 +52,47 @@ class Author{
 
 
 
-//class Book{
-//	
-//	private String name;
-//	Author author;
-//	private double price;
-//	private int qty;
-//	
-//	public String getName() {
-//		return name;
-//	}
-//	
-//	public Author getAuthor() {
-//		return author;
-//	}
-//	
-//	public double getPrice() {
-//		return price;
-//	}
-//	
-//	public void setPrice (double price) {
-//		this.price = price;
-//	}
-//	
-//	public int getQty() {
-//		return qty;
-//	}
-//	
-//	public void setQty(int qty) {
-//		this.qty = qty;
-//	}
-//	
-//	public String toString() {
-//		return "' "+name+" ' "+ "by " +author.getName() + " ("+ author.getGender()+") "+ " at "+author.getEmail();
-//
-//	}
-//	
-//}
+class Book{
+	
+	private String name;
+	private Author author;
+	private double price;
+	private int qty;
+	
+	public Book (String name, Author author, double price, int qty) {
+		this.name = name;
+		this.author = author;
+		this.price = price;
+		this.qty = qty;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public Author getAuthor() {
+		return author;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+	
+	public void setPrice (double price) {
+		this.price = price;
+	}
+	
+	public int getQty() {
+		return qty;
+	}
+	
+	public void setQty(int qty) {
+		this.qty = qty;
+	}
+	
+	public String toString() {
+		return "' "+name+" ' "+ "by " + author.toString();
+
+	}
+	
+}
